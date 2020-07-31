88a4fc72fb7807da74dac75087292c5ac77a8e03
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2020-07-31 22:22:36 +0530

    I have added Bottom Navigation bar and added fragments accordingly to the items as well.

--- app/src/main/java/com/example/bigbasket_user/MainActivity.java
+++ app/src/main/java/com/example/bigbasket_user/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.bigbasket_user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import Fragments.AccountFragment;
+import Fragments.CategoryFragment;
+import Fragments.HomeFragment;
+import Fragments.OfferFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView btm_navigation;
+    private Fragment selectorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +25,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Hello there");
+
+        btm_navigation = findViewById(R.id.btm_navigation);
+
+        btm_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.home_btm:
+                    selectorFragment= new HomeFragment();
+                    break;
+
+                    case R.id.categories_btm:
+                        selectorFragment= new CategoryFragment();
+                        break;
+
+                    case R.id.offers_btm:
+                        selectorFragment= new OfferFragment();
+                        break;
+
+                    case R.id.accounts_btm:
+                        selectorFragment= new AccountFragment();
+                        break;
+                }
+
+                if (selectorFragment!= null){
+                    //Replacing the fragments accordingly.
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectorFragment).commit();
+                }
+
+                return true;
+            }
+        });
+
+
     }
 }
