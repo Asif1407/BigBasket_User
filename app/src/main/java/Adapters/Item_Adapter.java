b793cf5a3443c31dda399e054a4a740b6c2b5ad0
Asif1407 <47074816+Asif1407@users.noreply.github.com>
2020-08-16 14:50:35 +0530

    Added categories fragment and item adapter

---
+++ app/src/main/java/Adapters/Item_Adapter.java
@@ -0,0 +1,80 @@
+package Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bigbasket_user.R;
+
+import java.util.ArrayList;
+
+import DataModels.Item;
+
+public class Item_Adapter extends RecyclerView.Adapter<Item_Adapter.MyViewHolder> {
+
+    ArrayList<Item> Items;
+    Context context;
+
+    public Item_Adapter(Context context, ArrayList<Item> itemNames) {
+        this.context = context;
+        this.Items = itemNames;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_adapter, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+//        holder.name.setText(itemNames.get(position));
+
+        holder.description.setText(Items.get(position).getDescription());
+        holder.price.setText(Integer.toString(Items.get(position).getPrice()));
+
+
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // display a toast with person name on item click
+                            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return Items.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title,description,price;
+        Button addtocart,buynow;// init the item view's
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            // get the reference of item view's
+//            name = (TextView) itemView.findViewById(R.id.name);
+            
+            description = itemView.findViewById(R.id.item_description);
+            price = itemView.findViewById(R.id.item_price);
+            addtocart = itemView.findViewById(R.id.addtocart_button);
+            buynow = itemView.findViewById(R.id.buynow_button);
+
+        }
+    }
+}
--- app/src/main/java/Fragments/CategoryFragment.java
+++ app/src/main/java/Fragments/CategoryFragment.java
@@ -3,17 +3,34 @@ package Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bigbasket_user.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.GithubAuthCredential;
+
+import java.util.ArrayList;
+
+import Adapters.Item_Adapter;
+import DataModels.Item;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CategoryFragment extends Fragment {
+
+
+    ArrayList<Item> Items = new ArrayList<>();
+    TabLayout tabLayout;
+    ArrayList<String> imageurls = new ArrayList<>();
+
+
 
     public CategoryFragment() {
         // Required empty public constructor
@@ -23,7 +40,53 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        tabLayout = view.findViewById(R.id.categorytab);
+
+
+        final RecyclerView recyclerView = view.findViewById(R.id.itemsRecyclerView);
+        // set a LinearLayoutManager with default vertical orientation
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        final Item_Adapter ItemAdapter = new Item_Adapter(getContext(),Items);
+
+        Item item = new Item("Title","Description",56,"yes","yes","url",imageurls);
+        Item item1 = new Item("Title","Description",56,"yes","yes","url",imageurls);
+        Items.add(item);
+        Items.add(item1);
+        ItemAdapter.notifyDataSetChanged();
+
+        recyclerView.setAdapter(ItemAdapter);
+
+
+
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition()==1){
+
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        return (view);
+
+
     }
 }
