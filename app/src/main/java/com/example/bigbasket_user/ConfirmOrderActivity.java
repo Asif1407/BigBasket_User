3a92530d194cb7e6759140e437f53f06f468126f
Pratik Patle <44264689+pratik2550@users.noreply.github.com>
2020-08-20 12:40:05 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/bigbasket_user/ConfirmOrderActivity.java
@@ -0,0 +1,44 @@
+package com.example.bigbasket_user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ConfirmOrderActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_order);
+
+        toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.homeBtn:
+                        startActivity(new Intent(ConfirmOrderActivity.this, MainActivity.class));
+                        finish();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_conferm_order, menu);
+        return true;
+    }
+}
