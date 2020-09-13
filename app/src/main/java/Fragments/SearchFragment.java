cbcb736913d93606725a7fb21b1adc0049371959
Pratik Patle <44264689+pratik2550@users.noreply.github.com>
2020-09-13 23:00:57 +0530

    Search UI Added

--- app/src/main/java/Fragments/SearchFragment.java
+++ app/src/main/java/Fragments/SearchFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,7 +41,7 @@ public class SearchFragment extends Fragment {
 
     // Init.
     private EditText searchText;
-    private Button searchBtn;
+    private ImageButton searchBtn;
     private RecyclerView recyclerView;
 
     // RecyclerView
